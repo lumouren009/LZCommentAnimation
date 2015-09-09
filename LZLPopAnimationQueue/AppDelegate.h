df4e82317fa8ba96c74b05db07bd892c53608c76
lumouren009(卢政) <luzheng1208@163.com>
2015-09-09 20:14:25 +0800

    first commit

---
+++ LZLPopAnimationQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZLPopAnimationQueue
+//
+//  Created by 卢政 on 15/9/4.
+//  Copyright (c) 2015年 卢政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
